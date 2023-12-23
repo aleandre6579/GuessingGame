35281d12d44e6bf49b9f4ebb725cc942fbccd9dc
Alex Simon <aleandre6579.simon@gmail.com>
2023-12-23 18:01:10 +0700

    FE: made snow effect

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,42 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Background from './components/Background';
 
 function App() {
   const [count, setCount] = useState(0)
-  
-  const backgroundDiv = document.getElementById('circle')
-  for(let i = 0; i < 10; i++) {
-    const circle = document.createElement("span")
-    circle.classList.add("back-circle")
-    document.body.insertBefore(circle, backgroundDiv)
-  }
 
   return (
     <>
-      <div id='bckground'>
-        <div className='grid grid-rows-4 grid-flow-col gap-4'>
-          <span id='circle'></span>
-        </div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Background/>
+      
+      <div>
+        <button className='w-5 h-5'>
+          Easy
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
