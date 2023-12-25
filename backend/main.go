766d82ff4ccdb6fdf52239d9ceaeeab569caf737
Alex Simon <aleandre6579.simon@gmail.com>
2023-12-25 15:56:32 +0700

    BE: fixed little bug in main

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"context"
 	"guessing-game/app"
+	"guessing-game/config"
 )
 
 func main() {
-	game := app.Init(app.CreateConfig())
+	game := app.Init(config.CreateConfig())
 	game.Start(context.Background())
 }
