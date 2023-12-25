af7ed41bcddeafe830740c163fca7cf1f0daba20
Alex Simon <aleandre6579.simon@gmail.com>
2023-12-25 18:57:58 +0700

    FE: token debugging

--- react-frontend/src/pages/Home.tsx
+++ react-frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import DifficultyButton from '../components/DifficultyButton';
 import { Link } from "react-router-dom";
 import '../App.css'
+import { useAuth } from '../auth/authProvider';
 
 function App() {
 
   return (
     <>
       <div className='flex flex-col gap-10'>
+        <p>Token: {useAuth()?.token}</p>
         <Link to='/easy'><DifficultyButton text='Easy' color='cornflowerblue'></DifficultyButton></Link>
         <Link to='/medium'><DifficultyButton text='Medium' color='darksalmon'></DifficultyButton></Link>
         <Link to='/hard'><DifficultyButton text='Hard' color='indianred'></DifficultyButton></Link>
